2dd0f1ea8fc92f9a0e235ea99b5b5af4df487056
samtoddler <er.sureshprajapati@gmail.com>
2017-01-09 17:24:04 +0530

    Update cron_child_alert.sh

--- cron_child_alert.sh
+++ cron_child_alert.sh
@@ -2,21 +2,22 @@
 # this will print the long running child of cron
 # usage : cron_child_alert.sh <threshold in sec>
 
-#!/usr/bin/env bash
+
 threshold=$1
 status=0
 cronpid="$(pgrep -P 1 cron)"
 cronjobs="$(pstree -p $cronpid | sed 's/)/)\n/g' | grep -v cron | grep -wo '[0-9]*')"
-#echo $cronjobs
+
 for job in ${cronjobs[@]}
 do
-	#for job in ${cronjobs[@]}; do  ps -p "$job" -o etimes,cmd --no-headers|grep bash|awk '{print $1}'; done
-	ptime="$(ps -p $job -o etimes,cmd --no-headers|grep '/bin/sh'|awk '{print $1}'|tr -d "\n")"
+	 #TODO : read the job starttime from /proc
+	 ptime="$(ps -p $job -o etimes,cmd --no-headers|grep '/bin/sh'|awk '{print $1}'|tr -d "\n")"
 	if [ $ptime > $threshold ]
 	then
-		pcron="$(ps -p "$job" -o cmd --no-headers|grep '/bin/sh')"
+		#pcron="$(ps -p "$job" -o cmd --no-headers|grep '/bin/sh')"
+		pcommand="$(cat /proc/$job/cmdline)"
         status=1
-		echo "Long Running Cron Child $job : $pcron"
+		echo "Long Running Cron Child $job : $pcommand"
 	fi
 done
-exit $statuss
+exit $status
