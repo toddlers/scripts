72fc9f05b40fbe559ff351d170a46634b1c6aa9d
samtoddler <er.sureshprajapati@gmail.com>
2017-01-09 16:19:31 +0530

    Update cron_child_alert.sh

--- cron_child_alert.sh
+++ cron_child_alert.sh
@@ -2,19 +2,21 @@
 # this will print the long running child of cron
 # usage : cron_child_alert.sh <threshold in sec>
 
+#!/usr/bin/env bash
 threshold=$1
 status=0
 cronpid="$(pgrep -P 1 cron)"
 cronjobs="$(pstree -p $cronpid | sed 's/)/)\n/g' | grep -v cron | grep -wo '[0-9]*')"
+#echo $cronjobs
 for job in ${cronjobs[@]}
 do
-  ptime="$(ps -p $job -o etimes,cmd --no-headers|grep bash|awk '{print $1}'|tr -d "\n")"
-  if [ $ptime > $threshold ]
-  then
-    status=1
-    pcron="$(ps -p "$job" -o etimes,cmd --no-headers|grep bash)"
-    echo "Long Running Cron Child $pcron"
-  fi
+	#for job in ${cronjobs[@]}; do  ps -p "$job" -o etimes,cmd --no-headers|grep bash|awk '{print $1}'; done
+	ptime="$(ps -p $job -o etimes,cmd --no-headers|grep '/bin/sh'|awk '{print $1}'|tr -d "\n")"
+	if [ $ptime > $threshold ]
+	then
+		pcron="$(ps -p "$job" -o cmd --no-headers|grep '/bin/sh')"
+        status=1
+		echo "Long Running Cron Child $job : $pcron"
+	fi
 done
-
-exit $status
+exit $statuss
