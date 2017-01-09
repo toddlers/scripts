fcaf3088cf87ffe25d2a2bdc05a2a870ad713078
samtoddler <er.sureshprajapati@gmail.com>
2017-01-09 16:04:04 +0530

    Update cron_child_alert.sh

--- cron_child_alert.sh
+++ cron_child_alert.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# this will print the long running child of cron
+# usage : cron_child_alert.sh <threshold in sec>
+
 threshold=$1
 cronpid="$(pgrep -P 1 cron)"
 cronjobs="$(pstree -p $cronpid | sed 's/)/)\n/g' | grep -v cron | grep -wo '[0-9]*')"
