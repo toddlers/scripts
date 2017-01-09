22aa6ff9f46448347d5a7616a7946e0beb50c9ee
samtoddler <er.sureshprajapati@gmail.com>
2017-01-09 16:10:14 +0530

    Update cron_child_alert.sh

--- cron_child_alert.sh
+++ cron_child_alert.sh
@@ -3,6 +3,7 @@
 # usage : cron_child_alert.sh <threshold in sec>
 
 threshold=$1
+status=0
 cronpid="$(pgrep -P 1 cron)"
 cronjobs="$(pstree -p $cronpid | sed 's/)/)\n/g' | grep -v cron | grep -wo '[0-9]*')"
 for job in ${cronjobs[@]}
@@ -10,7 +11,10 @@ do
   ptime="$(ps -p $job -o etimes,cmd --no-headers|grep bash|awk '{print $1}'|tr -d "\n")"
   if [ $ptime > $threshold ]
   then
+    status=1
     pcron="$(ps -p "$job" -o etimes,cmd --no-headers|grep bash)"
     echo "Long Running Cron Child $pcron"
   fi
 done
+
+exit $status
