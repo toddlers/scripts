5d65575bc7cf915396c5954648f558e0e31b770c
samtoddler <er.sureshprajapati@gmail.com>
2017-01-09 16:01:06 +0530

    Create cron_child_alert.sh

---
+++ cron_child_alert.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+threshold=$1
+cronpid="$(pgrep -P 1 cron)"
+cronjobs="$(pstree -p $cronpid | sed 's/)/)\n/g' | grep -v cron | grep -wo '[0-9]*')"
+for job in ${cronjobs[@]}
+do
+  ptime="$(ps -p $job -o etimes,cmd --no-headers|grep bash|awk '{print $1}'|tr -d "\n")"
+  if [ $ptime > $threshold ]
+  then
+    pcron="$(ps -p "$job" -o etimes,cmd --no-headers|grep bash)"
+    echo "Long Running Cron Child $pcron"
+  fi
+done
