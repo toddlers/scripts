802a355c119d6d781624feff61c3cebb534ec686
Suresh Prajapati <er.sureshprajapati@gmail.com>
2017-03-20 14:42:59 +0530

    Update opsworks_add_perms.go

--- go/opsworks_add_perms.go
+++ go/opsworks_add_perms.go
@@ -6,9 +6,24 @@ import (
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/opsworks"
+	flag "github.com/ogier/pflag"
 	"log"
+	"os"
 	"unsafe"
 )
+
+var arn string
+
+func init() {
+	flag.StringVarP(&arn, "arn", "a", "", "IAM ARN")
+}
+
+func printUsage() {
+	fmt.Printf("Usage: %s [options]\n", os.Args[0])
+	fmt.Println("Options:")
+	flag.PrintDefaults()
+	os.Exit(1)
+}
 
 func check(err error) {
 	if err != nil {
@@ -25,11 +40,14 @@ func getSession() *session.Session {
 	return sess
 }
 
-const (
-	iamArn = "arn:aws:iam::503805144375:user/suresh.prajapati"
-)
 
 func main() {
+	flag.Parse()
+
+	if flag.NFlag() == 0 {
+		printUsage()
+	}
+
 	sess := getSession()
 	svc := opsworks.New(sess)
 	params := &opsworks.DescribeStacksInput{}
@@ -38,7 +56,7 @@ func main() {
 
 	for i := 0; i < len(resp.Stacks); i++ {
 		params := &opsworks.SetPermissionInput{
-			IamUserArn: aws.String(iamArn),
+			IamUserArn: aws.String(arn),
 			StackId:    aws.String(*resp.Stacks[i].StackId),
 			AllowSsh:   aws.Bool(true),
 			AllowSudo:  aws.Bool(true),
@@ -46,7 +64,7 @@ func main() {
 		setPerms, err := svc.SetPermission(params)
 		check(err)
 		if unsafe.Sizeof(setPerms) == 8 {
-			fmt.Printf("Added sudo permissions to stack : %s", *resp.Stacks[i].Name)
+			fmt.Printf("Added sudo permissions to stack : %s\n", *resp.Stacks[i].Name)
 		}
 	}
 }
